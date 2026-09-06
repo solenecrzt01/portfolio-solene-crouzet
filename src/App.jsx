f70565816284438d40705ba6ec9371978891a08d
solenecrzt01 <solenecrzt01@gmail.com>
2026-09-06 02:04:13 +0200

    Ajout police Montserrat ExtraBold

--- src/App.jsx
+++ src/App.jsx
@@ -797,7 +797,6 @@ export default function App() {
 /* ------------------------------------------------------------------ */
 /*  CSS                                                                 */
 /* ------------------------------------------------------------------ */
-@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap');
 
 const CSS = `
 html,
